f0b5cbe2f26d6ba66c90e2cf5c851599674328ab
mickael dangleterre <mdangleterre@ippon.fr>
2019-10-30 19:27:55 +0100

    New AWS Module : elasticache

---
+++ modules/AWS/Elasticache/vars.tf
@@ -0,0 +1,19 @@
+variable "elasticache" {
+  type = list(string)
+}
+
+variable "SecGroup" {
+  type = list(string)
+}
+
+variable "SubnetGroup" {
+  type = list(string)
+}
+
+variable "ParamGroup" {
+  type = list(string)
+}
+
+variable "RepGroup" {
+  type = list(string)
+}
