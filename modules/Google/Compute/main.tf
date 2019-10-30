14a1a84b018d15c75d2d2a4523eda5e7c5cc4be6
mickael dangleterre <mdangleterre@ippon.fr>
2019-10-30 12:41:11 +0100

    update for google compute module

--- modules/Google/Compute/main.tf
+++ modules/Google/Compute/main.tf
@@ -1,54 +1,101 @@
 resource "google_compute_address" "gcp_address" {
-  count        = "${length(var.address)}"
-  name         = "${var.prefix}-${lookup(var.address[count.index],"suffix_name")}-${lookup(var.address[count.index],"id")}-addr"
-  region       = "${var.region}"
-  project      = "${var.project}"
-  subnetwork   = "${var.subnetwork}"
-  address_type = "${lookup(var.address[count.index],"address_type")}"
-  address      = "${lookup(var.address[count.index],"address")}"
+  count        = length(var.address)
+  name         = "${var.prefix}-${lookup(var.address[count.index], "suffix_name")}-${lookup(var.address[count.index], "id")}-addr"
+  region       = var.region
+  project      = var.project
+  subnetwork   = var.subnetwork
+  address_type = lookup(var.address[count.index], "address_type")
+  address      = lookup(var.address[count.index], "address")
 }
 
 resource "google_compute_disk" "gcp_disk" {
-  count = "${length(var.disk)}"
-  name  = "${lookup(var.disk[count.index],"name")}"
-  type  = "${lookup(var.disk[count.index],"type")}"
-  zone  = "${var.zone}"
-  size  = "${lookup(var.disk[count.index],"size")}"
+  count = length(var.disk)
+  name  = lookup(var.disk[count.index], "name")
+  type  = lookup(var.disk[count.index], "type")
+  zone  = var.zone
+  size  = lookup(var.disk[count.index], "size")
 }
 
 resource "google_compute_attached_disk" "gcp_attached_disk" {
-  count       = "${length(var.disk)}"
-  disk        = "${element(google_compute_disk.gcp_disk.*.self_link,count.index)}"
-  instance    = "${element(google_compute_instance.google_Vms.*.id,lookup(var.disk[count.index],"instance_id"))}"
-  device_name = "${lookup(var.disk[count.index],"device_name")}"
-  project     = "${var.project}"
-  zone        = "${var.zone}"
+  count       = length(var.disk)
+  disk        = element(google_compute_disk.gcp_disk.*.self_link, count.index)
+  instance    = element(google_compute_instance.google_Vms.*.id, lookup(var.disk[count.index], "instance_id"))
+  device_name = lookup(var.disk[count.index], "device_name")
+  project     = var.project
+  zone        = var.zone
 }
 
 resource "google_compute_instance" "google_Vms" {
-  count               = "${length(var.Linux_Vms)}"
-  name                = "${var.prefix}-${lookup(var.Linux_Vms[count.index],"suffix_name")}-${lookup(var.Linux_Vms[count.index], "id")}"
-  project             = "${var.project}"
+  count               = length(var.Linux_Vms)
+  name                = "${var.prefix}-${lookup(var.Linux_Vms[count.index], "suffix_name")}-${lookup(var.Linux_Vms[count.index], "id")}"
+  project             = var.project
   zone                = "projects/${var.app_project}/zones/${var.zone}"
-  can_ip_forward      = "${lookup(var.Linux_Vms[count.index],"can_ip_forward")}"
-  deletion_protection = "${lookup(var.Linux_Vms[count.index],"deletion_protection")}"
-  machine_type        = "${lookup(var.Linux_Vms[count.index],"machine_type")}"
+  can_ip_forward      = lookup(var.Linux_Vms[count.index], "can_ip_forward")
+  deletion_protection = lookup(var.Linux_Vms[count.index], "deletion_protection")
+  machine_type        = lookup(var.Linux_Vms[count.index], "machine_type")
 
-  "boot_disk" {
-    auto_delete = "${lookup(var.Linux_Vms[count.index],"auto_delete")}"
-    device_name = "${var.prefix}-${lookup(var.Linux_Vms[count.index],"suffix_name")}-${lookup(var.Linux_Vms[count.index], "id")}-bootdisk"
-
-    initialize_params {
-      size  = "${lookup(var.Linux_Vms[count.index],"size")}"
-      type  = "${lookup(var.Linux_Vms[count.index],"type")}"
-      image = "${lookup(var.Linux_Vms[count.index],"image")}"
+  dynamic "boot_disk" {
+    for_each = lookup(var.Linux_Vms[count.index], "boot_disk")
+    content {
+      auto_delete = lookup(boot_disk.value, "auto_delete")
+      device_name = lookup(boot_disk.value, "device_name")
+      initialize_params {
+        size  = lookup(boot_disk.value, "size")
+        type  = lookup(boot_disk.value, "type")
+        image = lookup(boot_disk.value, "image")
+      }
     }
   }
 
-  "network_interface" {
-    network    = "${var.network}"
-    subnetwork = "${var.subnetwork}"
-    address    = "${element(google_compute_address.gcp_address.*.id,lookup(var.Linux_Vms[count.index],"address_id"))}"
+  dynamic "network_interface" {
+    for_each = lookup(var.Linux_Vms[count.index], "network_interface")
+    content {
+      network    = lookup(network_interface.value, "network")
+      subnetwork = lookup(network_interface.value, "subnetwork")
+      address    = lookup(network_interface.value, "address")
+    }
+  }
+
+  dynamic "attached_disk" {
+    for_each = lookup(var.Linux_Vms[count.index], "attached_disk")
+    content {
+      source                  = lookup(attached_disk.value, "source")
+      device_name             = lookup(attached_disk.value, "device_name", null)
+      mode                    = lookup(attached_disk.value, "mode", null)
+      disk_encryption_key_raw = lookup(attached_disk.value, "disk_encryption_key_raw", null)
+      kms_key_self_link       = lookup(attached_disk.value, "kms_key_self_link", null)
+    }
+  }
+
+  dynamic "service_account" {
+    for_each = lookup(var.Linux_Vms[count.index], "service_account")
+    content {
+      email  = lookup(service_account.value, "email")
+      scopes = [lookup(service_account.value, "scopes")]
+    }
+  }
+
+  dynamic "scheduling" {
+    for_each = lookup(var.Linux_Vms[count.index], "scheduling")
+    content {
+      preemptible         = lookup(scheduling.value,"preemptible")
+      on_host_maintenance = lookup(scheduling.value,"on_host_maintenance")
+      automatic_restart   = lookup(scheduling.value,"automatic_restart")
+      node_affinities {
+        key      = lookup(scheduling.value,"key")
+        operator = lookup(scheduling.value,"operator", "NOT")
+        values   = [lookup(scheduling.value,"values")]
+      }
+    }
+  }
+
+  dynamic "shielded_instance_config" {
+    for_each = lookup(var.Linux_Vms[count.index], shielded_instance_config)
+    content {
+      enable_secure_boot = lookup(shielded_instance_config.value,"enable_secure_boot",false)
+      enable_vtpm = lookup(shielded_instance_config.value,"enable_vtpm",true)
+      enable_integrity_monitoring = lookup(shielded_instance_config.value,"enable_integrity_monitoring",true)
+    }
   }
 
   metadata {
