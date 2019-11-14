03df33f7d5982697ddb0a4f21ac3a94f6ae29603
mickael dangleterre <mdangleterre@ippon.fr>
2019-11-14 12:03:05 +0100

    DigitalOcean - update for DatabaseCluster outputs

--- modules/DigitalOcean/Create-DatabaseCluster/outputs.tf
+++ modules/DigitalOcean/Create-DatabaseCluster/outputs.tf
@@ -9,11 +9,3 @@ output "do_db_cluster_name" {
 output "do_db_cluster_engine" {
   value = digitalocean_database_cluster.do_db_cluster.*.engine
 }
-
-output "do_db_user_role" {
-  value = digitalocean_database_user.do_db_user.*.role
-}
-
-output do_db_user_password {
-  value = digitalocean_database_user.do_db_user.*.password
-}
