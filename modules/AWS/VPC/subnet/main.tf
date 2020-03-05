5dcb737da815e6e7a52f7fc0262e5ad21d84da31
mickael dangleterre <mdangleterre@ippon.fr>
2020-03-06 00:30:12 +0100

    Update subnet module to randomize availability zone with availability_zone_id and data/aws_availability_zones

--- modules/AWS/VPC/subnet/main.tf
+++ modules/AWS/VPC/subnet/main.tf
@@ -1,8 +1,12 @@
+data "aws_availability_zones" "az" {
+  state = "available"
+}
+
 resource "aws_subnet" "subnet" {
   count                   = length(var.subnet)
   cidr_block              = lookup(var.subnet[count.index], "cidr_block")
   vpc_id                  = element(var.vpc_id, lookup(var.subnet[count.index], "vpc_id"))
-  availability_zone       = lookup(var.subnet[count.index], "availability_zone")
   map_public_ip_on_launch = lookup(var.subnet[count.index], "map_public_ip_on_launch", false)
+  availability_zone       = data.aws_availability_zones.az.names[lookup(var.subnet[count.index], "availability_zone_id")]
   tags                    = var.tags
 }
