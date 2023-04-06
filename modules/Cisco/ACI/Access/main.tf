784846582d634b022fa5d6c79902bd883366c490
mikamakusa <michael.dangleterre@gmail.com>
2023-04-06 10:30:18 +0200

    update Cisco ACI Access Terraform Module

--- modules/Cisco/ACI/Access/main.tf
+++ modules/Cisco/ACI/Access/main.tf
@@ -1,81 +1,168 @@
+resource "aci_attachable_access_entity_profile" "main" {
+  for_each    = var.access_generic
+  name        = each.key
+  annotation  = each.value.annotation
+  name_alias  = each.value.name_alias
+  description = each.value.description
+}
+
 resource "aci_access_generic" "main" {
-  count                               = length(var.access_generic)
-  attachable_access_entity_profile_dn = element(aci_attachable_access_entity_profile.main.*.id, lookup(var.access_generic[count.index], "attachable_access_entity_profile_id"))
-  name                                = lookup(var.access_generic[count.index], "name")
-  annotation                          = lookup(var.access_generic[count.index], "annotation", null)
-  description                         = lookup(var.access_generic[count.index], "description", null)
-  name_alias                          = lookup(var.access_generic[count.index], "name_alias")
+  for_each                            = var.access_generic
+  attachable_access_entity_profile_dn = aci_attachable_access_entity_profile.main[each.key].id
+  name                                = each.key
+  annotation                          = each.value.annotation
+  name_alias                          = each.value.name_alias
+  description                         = each.value.description
 }
 
 resource "aci_access_switch_policy_group" "main" {
-  name = ""
-  annotation = ""
-  description = ""
-  name_alias = ""
-}
-
-resource "aci_attachable_access_entity_profile" "main" {
-  count       = length(var.attachable_access_entity_profile)
-  name        = lookup(var.attachable_access_entity_profile[count.index], "name")
-  annotation  = lookup(var.attachable_access_entity_profile[count.index], "annotation", null)
-  name_alias  = lookup(var.attachable_access_entity_profile[count.index], "name_alias", null)
-  description = lookup(var.attachable_access_entity_profile[count.index], "description", null)
+  for_each    = var.access_generic
+  name        = each.key
+  annotation  = each.value.annotation
+  description = each.value.description
+  name_alias  = each.value.name_alias
 }
 
 resource "aci_cdp_interface_policy" "main" {
-  name = ""
+  for_each    = var.cdp_interface_policy
+  name        = each.key
+  admin_st    = each.value.admin_st
+  annotation  = each.value.annotation
+  description = each.value.description
+  name_alias  = each.value.name_alias
 }
 
-resource "aci_error_disable_recovery" "main" {}
+resource "aci_error_disable_recovery" "main" {
+  for_each            = var.error_disable_recovery
+  annotation          = each.value.annotation
+  err_dis_recov_intvl = each.value.err_dis_recov_intvl
+  name_alias          = each.value.name_alias
+  description         = each.value.description
+
+  dynamic "edr_event" {
+    for_each = each.value.edr_event
+    content {
+      event = edr_event.value.event
+      name  = each.key
+    }
+  }
+}
 
 resource "aci_fabric_if_pol" "main" {
-  name = ""
+  for_each      = var.fabric_if_pol
+  name          = each.key
+  annotation    = each.value.annotation
+  description   = each.value.description
+  auto_neg      = each.value.auto_neg == null ? "on" : each.value.auto_neg
+  fec_mode      = each.value.fec_mode == null ? "inherit" : each.value.fec_mode
+  link_debounce = tostring(each.value.link_debounce == null ? 100 : each.value.link_debounce)
+  name_alias    = each.value.name_alias
+  speed         = each.value.speed == null ? "inherit" : each.value.speed
 }
 
 resource "aci_l2_interface_policy" "main" {
-  name = ""
+  for_each    = var.l2_interface_policy
+  name        = each.key
+  annotation  = each.value.annotation
+  description = each.value.description
+  name_alias  = each.value.name_alias
+  qinq        = each.value.qinq == null ? "disabled" : each.value.qinq
+  vepa        = each.value.vepa == null ? "disabled" : each.value.vepa
+  vlan_scope  = each.value.vlan_scope == null ? "global" : each.value.vlan_scope
 }
 
 resource "aci_l3_domain_profile" "main" {
-  name = ""
+  for_each   = var.access_generic
+  name       = each.key
+  annotation = each.value.annotation
+  name_alias = each.value.name_alias
 }
 
 resource "aci_lacp_policy" "main" {
-  name = ""
+  for_each    = var.lacp_policy
+  name        = each.key
+  description = each.value.description
+  annotation  = each.value.annotation
+  ctrl        = each.value.ctrl == [] ? ["fast-sel-hot-stdby", "graceful-conv", "susp-individual"] : each.value.ctrl
+  max_links   = tostring(each.value.max_links == null ? 16 : each.value.max_links)
+  min_links   = tostring(each.value.min_links == null ? 1 : each.value.min_links)
+  mode        = each.value.mode == null ? "off" : each.value.mode
+  name_alias  = each.value.name_alias
 }
 
 resource "aci_leaf_access_bundle_policy_group" "main" {
-  name = ""
+  for_each    = var.leaf_access_bundle_policy_group
+  name        = each.key
+  annotation  = each.value.annotation
+  description = each.value.description
+  name_alias  = each.value.name_alias
+  lag_t       = each.value.lag_t == null ? "link" : each.value.lag_t
 }
 
 resource "aci_leaf_access_port_policy_group" "main" {
-  name = ""
+  for_each    = var.access_generic
+  name        = each.key
+  annotation  = each.value.annotation
+  description = each.value.description
+  name_alias  = each.value.name_alias
 }
 
 resource "aci_leaf_breakout_port_group" "main" {
-  name = ""
+  for_each    = var.leaf_breakout_port_group
+  name        = each.key
+  annotation  = each.value.annotation
+  brkout_map  = each.value.brkout_map == null ? "none" : each.value.brkout_map
+  name_alias  = each.value.name_alias
+  description = each.value.description
 }
 
 resource "aci_lldp_interface_policy" "main" {
-  name = ""
+  name        = ""
+  description = ""
+  annotation  = ""
+  name_alias  = ""
+  admin_rx_st = ""
+  admin_tx_st = ""
 }
 
 resource "aci_mcp_instance_policy" "main" {
-  key = ""
+  key              = ""
+  annotation       = ""
+  admin_st         = ""
+  description      = ""
+  name_alias       = ""
+  ctrl             = []
+  init_delay_time  = ""
+  loop_detect_mult = ""
+  loop_protect_act = ""
+  tx_freq          = ""
+  tx_freq_msec     = ""
 }
 
 resource "aci_miscabling_protocol_interface_policy" "main" {
-  name = ""
+  name        = ""
+  admin_st    = ""
+  description = ""
+  annotation  = ""
+  name_alias  = ""
 }
 
 resource "aci_physical_domain" "main" {
-  name = ""
+  for_each   = var.access_generic
+  name       = each.key
+  annotation = each.value.annotation
+  name_alias = each.value.name_alias
 }
 
 resource "aci_ranges" "main" {
   from         = ""
   to           = ""
   vlan_pool_dn = ""
+  alloc_mode   = ""
+  annotation   = ""
+  name_alias   = ""
+  role         = ""
+  description  = ""
 }
 
 resource "aci_rest_managed" "main" {
@@ -84,43 +171,101 @@ resource "aci_rest_managed" "main" {
 }
 
 resource "aci_spanning_tree_interface_policy" "main" {
-  name = ""
+  name        = ""
+  annotation  = ""
+  description = ""
+  name_alias  = ""
+  ctrl        = []
 }
 
 resource "aci_spine_port_policy_group" "main" {
-  name = ""
+  for_each    = var.access_generic
+  name        = each.key
+  description = each.value.description
+  annotation  = each.value.annotation
+  name_alias  = each.value.name_alias
 }
 
 resource "aci_spine_switch_policy_group" "main" {
-  name = ""
+  for_each    = var.access_generic
+  name        = each.key
+  description = each.value.description
+  annotation  = each.value.annotation
+  name_alias  = each.value.name_alias
 }
 
 resource "aci_vlan_pool" "main" {
-  alloc_mode = ""
-  name       = ""
+  alloc_mode  = ""
+  name        = ""
+  description = ""
+  annotation  = ""
+  name_alias  = ""
 }
 
 resource "aci_vmm_controller" "main" {
-  host_or_ip     = ""
-  name           = ""
-  root_cont_name = ""
-  vmm_domain_dn  = ""
+  host_or_ip          = ""
+  name                = ""
+  root_cont_name      = ""
+  vmm_domain_dn       = ""
+  annotation          = ""
+  dvs_version         = ""
+  inventory_trig_st   = ""
+  mode                = ""
+  msft_config_err_msg = ""
+  msft_config_issues  = []
+  n1kv_stats_mode     = ""
+  port                = ""
+  scope               = ""
+  stats_mode          = ""
+  seq_num             = ""
+  vxlan_depl_pref     = ""
 }
 
 resource "aci_vmm_credential" "main" {
   name          = ""
   vmm_domain_dn = ""
+  annotation    = ""
+  description   = ""
+  name_alias    = ""
+  pwd           = ""
+  usr           = ""
 }
 
 resource "aci_vmm_domain" "main" {
   name                = ""
   provider_profile_dn = ""
+  access_mode         = ""
+  annotation          = ""
+  arp_learning        = ""
+  enable_ave          = ""
+  ave_time_out        = ""
+  config_infra_pg     = ""
+  ctrl_knob           = ""
+  delimiter           = ""
+  enable_tag          = ""
+  enable_vm_folder    = ""
+  encap_mode          = ""
+  enf_pref            = ""
+  ep_inventory_type   = ""
+  ep_ret_time         = ""
+  hv_avail_monitor    = ""
+  mcast_addr          = ""
+  mode                = ""
+  name_alias          = ""
+  pref_encap_mode     = ""
 }
 
 resource "aci_vpc_domain_policy" "main" {
-  name = ""
+  name        = ""
+  annotation  = ""
+  dead_intvl  = ""
+  name_alias  = ""
+  description = ""
 }
 
 resource "aci_vswitch_policy" "main" {
   vmm_domain_dn = ""
+  annotation    = ""
+  description   = ""
+  name_alias    = ""
 }
--- modules/Cisco/ACI/Access/vars.tf
+++ modules/Cisco/ACI/Access/vars.tf
@@ -1,7 +1,161 @@
 variable "access_generic" {
-  type = any
+  type = map(object({
+    annotation  = optional(string)
+    description = optional(string)
+    name_alias  = optional(string)
+  }))
 }
 
-variable "attachable_access_entity_profile" {
-  type = any
+variable "cdp_interface_policy" {
+  type = map(object({
+    admin_st    = optional(string)
+    annotation  = optional(string)
+    description = optional(string)
+    name_alias  = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["enabled", "disabled"], var.cdp_interface_policy.admin_st)
+    error_message = "Allowed values: 'enabled', 'disabled'."
+  }
+}
+
+variable "error_disable_recovery" {
+  type = map(object({
+    annotation          = optional(string)
+    description         = optional(string)
+    name_alias          = optional(string)
+    err_dis_recov_intvl = optional(string)
+    edr_event = optional(object({
+      event = string
+    }))
+  }))
+
+  validation {
+    condition     = var.error_disable_recovery.err_dis_recov_intvl >= 30 && var.error_disable_recovery.err_dis_recov_intvl <= 65535
+    error_message = "Allowed range is '30' - '65535'."
+  }
+
+  validation {
+    condition     = contains(["event-arp-inspection", "event-bpduguard", "event-debug-1", "event-debug-2", "event-debug-3", "event-debug-4", "event-debug-5", "event-dhcp-rate-lim", "event-ep-move", "event-ethpm", "event-ip-addr-conflict", "event-ipqos-dcbxp-compat-failure", "event-ipqos-mgr-error", "event-link-flap", "event-loopback", "event-mcp-loop", "event-psec-violation", "event-sec-violation", "event-set-port-state-failed", "event-storm-ctrl", "event-stp-inconsist-vpc-peerlink", "event-syserr-based", "event-udld", "unknown"], var.error_disable_recovery.edr_event.event)
+    error_message = "Allowed values are 'event-arp-inspection', 'event-bpduguard', 'event-debug-1', 'event-debug-2', 'event-debug-3', 'event-debug-4', 'event-debug-5', 'event-dhcp-rate-lim', 'event-ep-move', 'event-ethpm', 'event-ip-addr-conflict', 'event-ipqos-dcbxp-compat-failure', 'event-ipqos-mgr-error', 'event-link-flap', 'event-loopback', 'event-mcp-loop', 'event-psec-violation', 'event-sec-violation', 'event-set-port-state-failed', 'event-storm-ctrl', 'event-stp-inconsist-vpc-peerlink', 'event-syserr-based', 'event-udld', 'unknown'."
+  }
+}
+
+variable "fabric_if_pol" {
+  type = map(object({
+    annotation    = optional(string)
+    description   = optional(string)
+    name_alias    = optional(string)
+    auto_neg      = optional(string)
+    fec_mode      = optional(string)
+    link_debounce = optional(number)
+    speed         = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["on", "off"], var.fabric_if_pol.auto_neg)
+    error_message = "Allowed values: 'on', 'off'."
+  }
+
+  validation {
+    condition     = contains(["inherit", "cl91-rs-fec", "cl74-fc-fec", "ieee-rs-fec", "cons16-rs-fec", "kp-fec", "disable-fec"], var.fabric_if_pol.fec_mode)
+    error_message = "Allowed values: 'inherit', 'cl91-rs-fec', 'cl74-fc-fec', 'ieee-rs-fec', 'cons16-rs-fec', 'kp-fec', 'disable-fec'."
+  }
+
+  validation {
+    condition     = var.fabric_if_pol.link_debounce >= 0 && var.fabric_if_pol.link_debounce <= 5000
+    error_message = "Range of allowed values: '0' to '5000'."
+  }
+
+  validation {
+    condition     = contains(["unknown", "100M", "1G", "10G", "25G", "40G", "50G", "100G", "200G", "400G", "inherit"], var.fabric_if_pol.speed)
+    error_message = "Allowed values : 'unknown', '100M', '1G', '10G', '25G', '40G', '50G', '100G', '200G', '400G', 'inherit'."
+  }
+}
+
+variable "l2_interface_policy" {
+  type = map(object({
+    annotation  = optional(string)
+    description = optional(string)
+    name_alias  = optional(string)
+    qinq        = optional(string)
+    vepa        = optional(string)
+    vlan_scope  = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["disabled", "edgePort", "corePort", "doubleQtagPort"], var.l2_interface_policy.qinq)
+    error_message = "Allowed values are 'disabled', 'edgePort', 'corePort' and 'doubleQtagPort'."
+  }
+
+  validation {
+    condition     = contains(["enabled", "disabled"], var.l2_interface_policy.vepa)
+    error_message = "Allowed values: 'enabled', 'disabled'."
+  }
+
+  validation {
+    condition     = contains(["global", "portlocal"], var.l2_interface_policy.vlan_scope)
+    error_message = "Allowed values: 'global', 'portlocal'."
+  }
+}
+
+variable "lacp_policy" {
+  type = map(object({
+    description = optional(string)
+    annotation  = optional(string)
+    name_alias  = optional(string)
+    ctrl        = optional(list(string))
+    max_links   = optional(string)
+    min_links   = optional(string)
+    mode        = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["symmetric-hash", "susp-individual", "graceful-conv", "load-defer", "fast-sel-hot-stdby"], var.lacp_policy.ctrl)
+    error_message = "Allowed values : 'symmetric-hash', 'susp-individual', 'graceful-conv', 'load-defer' and 'fast-sel-hot-stdby'."
+  }
+
+  validation {
+    condition     = contains(["off", "active", "passive", "mac-pin", "mac-pin-unload"], var.lacp_policy.mode)
+    error_message = "Allowed values : 'off', 'active', 'passive', 'mac-pin' and 'mac-pin-unload'."
+  }
+
+  validation {
+    condition     = var.lacp_policy.max_links >= 1 && var.lacp_policy.max_links <= 16
+    error_message = "Allowed range : 1 to 16."
+  }
+
+  validation {
+    condition     = var.lacp_policy.min_links >= 1 && var.lacp_policy.min_links <= 16
+    error_message = "Allowed range : 1 to 16."
+  }
+}
+
+variable "leaf_access_bundle_policy_group" {
+  type = map(object({
+    annotation  = optional(string)
+    description = optional(string)
+    name_alias  = optional(string)
+    lag_t       = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["not-aggregated", "node", "link"], var.leaf_access_bundle_policy_group.lag_t)
+    error_message = "Allowed values : 'not-aggregated', 'node', 'link'."
+  }
+}
+
+variable "leaf_breakout_port_group" {
+  type = map(object({
+    annotation  = optional(string)
+    description = optional(string)
+    name_alias  = optional(string)
+    brkout_map  = optional(string)
+  }))
+
+  validation {
+    condition     = contains(["100g-2x", "100g-4x", "10g-4x", "25g-4x", "50g-8x", "none"], var.leaf_breakout_port_group.brkout_map)
+    error_message = "Allowed values : '100g-2x', '100g-4x', '10g-4x', '25g-4x', '50g-8x' and 'none'."
+  }
 }
