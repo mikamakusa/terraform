91ab2cb58c0c9448445b33952431ddaed868fbe1
Mickael Dangleterre <michael.dangleterre@gmail.com>
2024-05-20 18:32:04 +0200

    update

--- modules/DigitalOcean/Create_ContainerRegistry/main.tf
+++
@@ -1,4 +0,0 @@
-resource "digitalocean_container_registry" "container_registry" {
-  count = length(var.container_registry)
-  name = lookup(var.container_registry[count.index], "name")
-}
--- modules/DigitalOcean/Create_ContainerRegistry/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "container_registry" {
-  type = list
-}
